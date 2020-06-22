bf8bc86b5829a9c4d220a20126c73b658f0c2fc1
muhammad elsaied <mohsfathy@outlook.com>
2020-06-22 21:23:29 +0200

    initialize app

---
+++ simpleCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleCounter
+//
+//  Created by Muhammad Elsaied on 6/22/20.
+//  Copyright © 2020 Muhammad Elsaied. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
