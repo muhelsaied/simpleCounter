13a883ec8aabc7636f35f71eb1eac8aead8cfb09
muhammad elsaied <mohsfathy@outlook.com>
2020-06-22 22:07:16 +0200

    setup app

--- simpleCounter/ViewController.swift
+++ simpleCounter/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var counter : Int = 0
 
+    var timeCounter : Int  = 0
+    @IBOutlet weak var mainView : UIImageView!
+    @IBOutlet weak var timeCounterLbl : UILabel!
+    @IBOutlet weak var counterLbl : UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 50.0
+        mainView.clipsToBounds = true
+        
+         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+            mainView.isUserInteractionEnabled = true
+            mainView.addGestureRecognizer(tapGestureRecognizer)
+        }
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+          {
+//              let tappedImage = tapGestureRecognizer.view as! UIImageView
+            if counter == 100 {
+                counter = 0
+                timeCounter = timeCounter + 1
+            }
+            counter = counter + 1
+            counterLbl.text = "\(counter)"
+            timeCounterLbl.text = "\(timeCounter)"
+          }
     }
-
-
-}
+   
 
